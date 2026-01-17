f99869a38724b65fce11601bc2a3cebf7019a3c5
Lena <braggslena@gmail.com>
2026-01-17 17:20:53 -0500

    Builds without errors

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026_robot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "b672ea57cdb3acd2ba7389cca602131a25026078";
+  public static final String GIT_DATE = "2026-01-17 15:32:31 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-17 17:20:11 EST";
+  public static final long BUILD_UNIX_TIME = 1768688411825L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
