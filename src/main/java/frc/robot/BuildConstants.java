863a343439aad9b733944e76fca0b44e3324cf04
Lena <braggslena@gmail.com>
2026-02-07 10:28:59 -0500

    update Phoenix6

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "9adadd835e7d581e459f88463a187401048b5a10";
-  public static final String GIT_DATE = "2026-01-24 17:44:48 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "20478f702ec5a6194aeb1d32818000c9284e0a50";
+  public static final String GIT_DATE = "2026-01-31 15:45:17 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-31 15:41:36 EST";
-  public static final long BUILD_UNIX_TIME = 1769892096717L;
+  public static final String BUILD_DATE = "2026-02-07 10:28:02 EST";
+  public static final long BUILD_UNIX_TIME = 1770478082857L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
