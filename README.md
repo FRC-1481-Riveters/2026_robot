acac39504fe2ac7147c2f2cca88b1fabfaca983b
Lena <braggslena@gmail.com>
2026-02-09 18:37:17 -0500

    switch back to CTRE swerve

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 woot new robot
+
+todo:
+- Use Phoenix Tuner to generate swerve code
+- - need measurements
+- Add stuff from last year (logging, AdvantageKit, Limelight)
+- Support Intake and Shooter as needed
+
+- get 6 GHz wifi working
--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "20478f702ec5a6194aeb1d32818000c9284e0a50";
-  public static final String GIT_DATE = "2026-01-31 15:45:17 EST";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "863a343439aad9b733944e76fca0b44e3324cf04";
+  public static final String GIT_DATE = "2026-02-07 10:28:59 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-07 10:28:02 EST";
-  public static final long BUILD_UNIX_TIME = 1770478082857L;
+  public static final String BUILD_DATE = "2026-02-09 18:34:22 EST";
+  public static final long BUILD_UNIX_TIME = 1770680062382L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
