aca64a775b04bc9a4340a955b9476a569a605aff
Neal <Nyxyxylyth@users.noreply.github.com>
2026-03-17 16:55:17 -0400

    more cleanup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,6 @@ import com.ctre.phoenix6.HootAutoReplay;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobotBase;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Watchdog;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
