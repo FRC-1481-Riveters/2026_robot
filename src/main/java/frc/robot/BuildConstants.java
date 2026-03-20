035e2074e54915043f3634bcddc564effa8a7609
Lena <braggslena@gmail.com>
2026-03-19 23:31:39 -0400

    updated AdvantageKit and Phoenix6

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 51;
-  public static final String GIT_SHA = "367a92e9d70c7251a1fab6d82d82e72505ec45ec";
-  public static final String GIT_DATE = "2026-03-19 23:11:30 EDT";
+  public static final int GIT_REVISION = 52;
+  public static final String GIT_SHA = "b819af48de922415fcfd50735d93154ad115c5a5";
+  public static final String GIT_DATE = "2026-03-19 23:23:42 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-19 23:14:02 EDT";
-  public static final long BUILD_UNIX_TIME = 1773976442355L;
+  public static final String BUILD_DATE = "2026-03-19 23:30:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1773977419205L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
