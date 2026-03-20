b819af48de922415fcfd50735d93154ad115c5a5
Lena <braggslena@gmail.com>
2026-03-19 23:23:42 -0400

    tweaked center crossing to be farther from trench

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 41;
-  public static final String GIT_SHA = "cbe376f6132c1f2715c68b83c54b1c31b618c026";
-  public static final String GIT_DATE = "2026-03-19 18:34:22 EDT";
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "367a92e9d70c7251a1fab6d82d82e72505ec45ec";
+  public static final String GIT_DATE = "2026-03-19 23:11:30 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-19 23:06:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1773975982526L;
+  public static final String BUILD_DATE = "2026-03-19 23:14:02 EDT";
+  public static final long BUILD_UNIX_TIME = 1773976442355L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
