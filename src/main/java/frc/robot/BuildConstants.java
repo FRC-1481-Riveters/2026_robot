9b4b4de91eee8b772d276302f75b316383daa17e
Lena <braggslena@gmail.com>
2026-04-01 21:50:47 -0400

    updoot

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -11,9 +11,9 @@ public final class BuildConstants {
   public static final String GIT_SHA = "8bec05b637f2d0527cb26e5368029cff8613ef7b";
   public static final String GIT_DATE = "2026-03-31 22:32:39 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-04-01 21:05:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1775091959184L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-04-01 21:07:37 EDT";
+  public static final long BUILD_UNIX_TIME = 1775092057827L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
